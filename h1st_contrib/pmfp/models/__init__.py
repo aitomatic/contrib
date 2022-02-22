656351ff743c48ac23ac0b7eec61141ff45faa48
The Vinh LUONG (LƯƠNG Thế Vinh) <TheVinhLuong@gmail.com>
2022-02-21 16:19:10 -0800

    refactor h1st_contrib.pmfp.models

--- h1st_contrib/pmfp/models/__init__.py
+++ h1st_contrib/pmfp/models/__init__.py
@@ -1,15 +1,17 @@
 """H1st Modelers & Models."""
 
 
-from .base import H1ST_MODELS_S3_DIR_PATH, H1ST_BATCH_OUTPUT_S3_DIR_PATH
+from .base import (BaseFaultPredictor,
+                   H1ST_MODELS_S3_DIR_PATH, H1ST_BATCH_OUTPUT_S3_DIR_PATH)
 from .oracle.teacher.base import BaseFaultPredTeacher
 from .oracle.student.timeseries_dl import TimeSeriesDLFaultPredStudent
 from .oracle import FaultPredOracle
 
 
 __all__ = (
-    'H1ST_MODELS_S3_DIR_PATH', 'H1ST_BATCH_OUTPUT_S3_DIR_PATH',
+    'BaseFaultPredictor',
     'BaseFaultPredTeacher',
     'TimeSeriesDLFaultPredStudent',
     'FaultPredOracle',
+    'H1ST_MODELS_S3_DIR_PATH', 'H1ST_BATCH_OUTPUT_S3_DIR_PATH',
 )
