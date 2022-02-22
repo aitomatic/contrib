87ad5744740e8085eea7618f77fe789378a8650c
The Vinh LUONG (LƯƠNG Thế Vinh) <TheVinhLuong@gmail.com>
2022-02-21 22:15:34 -0800

    add 1st-cut h1st_contrib.pmfp.cli.oracle

--- h1st_contrib/pmfp/cli/oracle/tune.py
+++ h1st_contrib/pmfp/cli/oracle/tune.py
@@ -18,11 +18,78 @@ from h1st_contrib.pmfp.models import TimeSeriesDLFaultPredStudent
 import h1st_contrib.utils.debug
 
 
-@click.command(name='tune-fault-pred-student-decision-threshold')
-def tune_fault_pred_student_decision_threshold(
-        student_version: str,
-        date_range: Tuple[str, str],
-        debug: bool = False):
+@click.command(name='tune-fault-pred-student-decision-threshold',
+               cls=click.Command,
+
+               # Command kwargs
+               context_settings=None,
+               # callback=None,
+               # params=None,
+               help="Tune a Student model's decision threshold >>>",   # noqa: E501
+               epilog="^^^ Tune a Student model's decision threshold",  # noqa: E501
+               short_help="Tune a Student model's decision threshold",  # noqa: E501
+               options_metavar='[OPTIONS]',
+               add_help_option=True,
+               no_args_is_help=True,
+               hidden=False,
+               deprecated=False)
+@click.argument('student_version',
+                type=str,
+                required=True,
+                default=None,
+                callback=None,
+                nargs=None,
+                # multiple=False,
+                metavar='STUDENT_VERSION',
+                expose_value=True,
+                is_eager=False,
+                envvar=None,
+                shell_complete=None,
+                autocompletion=None)
+@click.argument('date_range',
+                type=str,
+                required=True,
+                default=None,
+                callback=None,
+                nargs=2,
+                # multiple=False,
+                metavar='FROM_DATE TO_DATE',
+                expose_value=True,
+                is_eager=False,
+                envvar=None,
+                shell_complete=None,
+                autocompletion=None)
+@click.option('--debug',
+              show_default=True,
+              prompt=False,
+              confirmation_prompt=False,
+              prompt_required=True,
+              hide_input=False,
+              is_flag=True,
+              flag_value=True,
+              multiple=False,
+              count=False,
+              allow_from_autoenv=True,
+              help='Run in DEBUG mode',
+              hidden=False,
+              show_choices=True,
+              show_envvar=False,
+
+              type=bool,
+              required=False,
+              default=False,
+              callback=None,
+              nargs=None,
+              # multiple=False,
+              metavar='DEBUG',
+              expose_value=True,
+              is_eager=False,
+              envvar=None,
+              shell_complete=None,
+              autocompletion=None)
+def tune_fault_pred_student_decision_threshold(student_version: str,
+                                               date_range: Tuple[str, str],
+                                               debug: bool = False):
     """Tune a Knowledge Generalizer ("Student") model's decision threshold."""
     if debug:
         h1st_contrib.utils.debug.ON = True
