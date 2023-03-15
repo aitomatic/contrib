2ed825f06a120c2028b63792babb9d3e4bc1b925
The Vinh LUONG (LƯƠNG Thế Vinh) <TheVinhLuong@gmail.com>
2023-03-14 22:50:17 -0700

    add h1st_contrib.img_classify.imagenet.classify_based_on_imagenet_similarity

--- h1st_contrib/img_classif/imagenet.py
+++ h1st_contrib/img_classif/imagenet.py
@@ -3,7 +3,7 @@
 
 import json
 from pathlib import Path
-from typing import Sequence, Set, Union
+from typing import Dict, List, Sequence, Set, Union  # Py3.9+: use built-ins
 
 from transformers.pipelines import pipeline
 from transformers.pipelines.image_classification import \
@@ -12,7 +12,10 @@ from transformers.pipelines.image_classification import \
 from .util import ImgInputType, ImgClassifType
 
 
-__all__: Sequence[str] = ('imagenet_classify',)
+__all__: Sequence[str] = (
+    'imagenet_classify',
+    'classify_based_on_imagenet_similarity',
+)
 
 
 IMAGENET_CLASSES_FILE_NAME: str = 'ImageNet-Classes.json'
@@ -59,3 +62,23 @@ def imagenet_classify(
                  f'{set(imagenet_classif).difference(IMAGENET_CLASSES)} ***')
 
     return imagenet_classif
+
+
+def classify_based_on_imagenet_similarity(
+        img_input: Union[ImgInputType, Sequence[ImgInputType]],
+        classes_mapped_to_similar_imagenet_classes: Dict[str, List[str]], /) \
+        -> Union[ImgClassifType, Sequence[ImgClassifType]]:
+    """Classify target classes based on mapping from such classes to ImageNet."""  # noqa: E501
+    return ([{target_class: sum(i[imagenet_class]
+                                for imagenet_class in imagenet_classes)
+              for target_class, imagenet_classes
+              in classes_mapped_to_similar_imagenet_classes.items()}
+             for i in imagenet_classif]
+
+            if isinstance(imagenet_classif := imagenet_classify(img_input),
+                          (list, tuple))
+
+            else {target_class: sum(imagenet_classif[imagenet_class]
+                                    for imagenet_class in imagenet_class_names)
+                  for target_class, imagenet_class_names
+                  in classes_mapped_to_similar_imagenet_classes.items()})
