f95f00a1444e5df60462baad4620d0def08b056d
The Vinh LUONG (LƯƠNG Thế Vinh) <TheVinhLuong@gmail.com>
2023-03-14 22:51:13 -0700

    update h1st_contrib.img_classif

--- h1st_contrib/img_classif/__init__.py
+++ h1st_contrib/img_classif/__init__.py
@@ -3,11 +3,11 @@
 
 from typing import Sequence
 
-from .imagenet_classifier import imagenet_classify
+from .imagenet import imagenet_classify, classify_based_on_imagenet_similarity
 from .util import ImgInputType, ImgClassifType
 
 
 __all__: Sequence[str] = (
-    'imagenet_classify',
+    'imagenet_classify', 'classify_based_on_imagenet_similarity',
     'ImgInputType', 'ImgClassifType',
 )
