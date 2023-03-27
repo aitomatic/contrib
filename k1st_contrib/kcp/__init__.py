54c7b797ec7537acb98ade19850dab7e253aab7d
The Vinh LUONG (LƯƠNG Thế Vinh) <TheVinhLuong@gmail.com>
2023-03-27 07:23:08 -0700

    add place-holder k1st_contrib.kcp.KCP

---
+++ k1st_contrib/kcp/__init__.py
@@ -0,0 +1,8 @@
+"""Knowledge-Conditioned Probabilities (k-CP)."""
+
+
+class KCP:  # pylint: disable=too-few-public-methods
+    """Knowledge-Conditioned Probabilities (k-CP)."""
+
+    def __init__(self):
+        """Initialize."""
