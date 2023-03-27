05931de855592076ad3b9d15ba404a34b1b6d7eb
The Vinh LUONG (LƯƠNG Thế Vinh) <TheVinhLuong@gmail.com>
2023-03-27 09:53:26 -0700

    refactor k1st_contrib.kcp.base.KCP

--- k1st_contrib/kcp/base.py
+++ k1st_contrib/kcp/base.py
@@ -1,6 +1,7 @@
 """Knowledge-Conditioned Probabilities (k-CP)."""
 
 
+from math import prod
 from typing import Sequence, Union
 
 from ..util.prob import normalize
@@ -10,24 +11,24 @@ from ..util.types import ClassifType
 class KCP:  # pylint: disable=too-few-public-methods
     """Knowledge-Conditioned Probabilities (k-CP)."""
 
-    def __init__(self, k_contrib: float = 0.68):
-        """Initialize."""
-        self.k_contrib: float = k_contrib
-        self.ml_contrib: float = 1 - k_contrib
-
     def __call__(self,
-                 k_classif: Union[ClassifType, Sequence[ClassifType]],
-                 ml_classif: Union[ClassifType, Sequence[ClassifType]]) \
+                 classif_0: Union[ClassifType, Sequence[ClassifType]], /,
+                 *other_classifs: Union[ClassifType, Sequence[ClassifType]]) \
             -> Union[ClassifType, Sequence[ClassifType]]:
         """Fuse 2 sets of probabilities."""
-        if isinstance(k_classif, (list, tuple)):
-            assert (isinstance(ml_classif, (list, tuple)) and
-                    (len(ml_classif) == (n := len(k_classif)))), \
-                f'*** ml_classif NOT A SEQUENCE OF LENGTH {n} ***'
+        if isinstance(classif_0, (list, tuple)):
+            assert (n := len(classif_0)) > 0
 
-            return [normalize({class_name: (k_prob * d1[class_name])
-                               for class_name, k_prob in d0.items()})
-                    for d0, d1 in zip(k_classif, ml_classif)]
+            for classif in other_classifs:
+                assert (isinstance(classif, (list, tuple)) and
+                        (len(classif) == n)), \
+                    f'*** {classif} NOT A SEQUENCE OF LENGTH {n} ***'
 
-        return normalize({class_name: (k_prob * ml_classif[class_name])
-                          for class_name, k_prob in k_classif.items()})
+            return [normalize({class_name: prod(classif[class_name]
+                                                for classif in classifs)
+                               for class_name in classif_0})
+                    for classifs in zip(classif_0, *other_classifs)]
+
+        return normalize({class_name: prod(classif[class_name]
+                                           for classif in (classif_0, *other_classifs))  # noqa:E501
+                          for class_name in classif_0})
--- k1st_contrib/kcp/cv.py
+++ k1st_contrib/kcp/cv.py
@@ -10,16 +10,11 @@ from ..util.types import ClassifType
 class KCPCV:  # pylint: disable=too-few-public-methods
     """Knowledge-Conditioned Probabilities (k-CP)."""
 
-    def __init__(self, k_contrib: float = 0.68):
-        """Initialize."""
-        self.k_contrib: float = k_contrib
-        self.ml_contrib: float = 1 - k_contrib
-
     def __call__(self,
                  k_classif: Union[ClassifType, Sequence[ClassifType]],
                  ml_classif: Union[ClassifType, Sequence[ClassifType]]) \
             -> Union[ClassifType, Sequence[ClassifType]]:
-        """Fuse 2 sets of probabilities."""
+        """Fuse multiple sets of probabilities."""
         if isinstance(k_classif, (list, tuple)):
             assert (isinstance(ml_classif, (list, tuple)) and
                     (len(ml_classif) == (n := len(k_classif)))), \
