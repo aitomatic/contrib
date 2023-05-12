23097afda5b386b6027fbedb71abbe029bef0d80
The Vinh LUONG (LƯƠNG Thế Vinh) <TheVinhLuong@gmail.com>
2023-05-12 06:08:20 -0700

    add k1st_contrib.aicalm.agents.time_series_analyzer.TimeSeriesAnalyzer

---
+++ k1st_contrib/aicalm/agents/__init__.py
@@ -0,0 +1,11 @@
+"""AI-CALM Agents."""
+
+
+from collections.abc import Sequence
+
+from .time_series_analyzer import TimeSeriesAnalyzer
+
+
+__all__: Sequence[str] = (
+    'TimeSeriesAnalyzer',
+)
---
+++ k1st_contrib/aicalm/agents/time_series_analyzer/__init__.py
@@ -0,0 +1,21 @@
+"""Time Series Analyzer (TSA)."""
+
+
+from pandas.core.frame import DataFrame
+from pandasai import PandasAI
+from pandasai.llm.base import LLM
+
+
+class TimeSeriesAnalyzer:  # pylint: disable=too-few-public-methods
+    """Time Series Analyzer (TSA)."""
+
+    def __init__(self, llm: LLM):
+        """Initialize TSA."""
+        self.pandas_ai: PandasAI = PandasAI(llm=llm,
+                                            conversational=True,
+                                            verbose=False,
+                                            enforce_privacy=False)
+
+    def analyze(self, df: DataFrame, /, prompt: str):
+        """Analyze tabular/time-series data frame."""
+        return self.pandas_ai.run(data_frame=df, prompt=prompt)
