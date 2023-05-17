63071986c453e207893561aa3b44b4b0b5729eb4
The Vinh LUONG (LƯƠNG Thế Vinh) <TheVinhLuong@gmail.com>
2023-05-17 13:40:40 -0700

    enhance TSA agent

--- aicalm/agents/time_series_analyzer/__init__.py
+++ aicalm/agents/time_series_analyzer/__init__.py
@@ -9,13 +9,18 @@ from pandasai.llm.base import LLM
 class TimeSeriesAnalyzer:  # pylint: disable=too-few-public-methods
     """Time Series Analyzer (TSA)."""
 
-    def __init__(self, llm: LLM):
+    def __init__(self, llm: LLM, verbose: bool = False):
         """Initialize TSA."""
         self.pandas_ai: PandasAI = PandasAI(llm=llm,
                                             conversational=True,
-                                            verbose=False,
+                                            verbose=verbose,
                                             enforce_privacy=False)
 
-    def __call__(self, df: DataFrame, /, prompt: str):
+    def __call__(self, df: DataFrame, /, prompt: str,
+                 *, conversational: bool = True):
         """Analyze tabular/time-series data frame."""
-        return self.pandas_ai.run(data_frame=df, prompt=prompt)
+        return self.pandas_ai.run(data_frame=df, prompt=prompt,
+                                  is_conversational_answer=conversational,
+                                  show_code=True,
+                                  anonymize_df=True,
+                                  use_error_correction_framework=True)
